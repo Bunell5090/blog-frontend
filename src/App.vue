c7b0f293c3e974f003b4cb56842e2b708801a9cd
Bunell5090 <Bunell5090.com>
2022-04-12 19:39:29 -0500

    Added bootstrap navbar to top of page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <!-- <router-link to="/">Home</router-link>
     |
     <router-link to="/about">About</router-link>
     |
@@ -14,10 +14,10 @@
     |
     <router-link to="/posts">View Posts</router-link>
     |
-    <router-link to="/posts/new">New Post</router-link>
+    <router-link to="/posts/new">New Post</router-link> -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
         <button
           class="navbar-toggler"
           type="button"
@@ -32,33 +32,44 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+              <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/test">Test</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/posts">View Posts</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/posts/new">New Posts</a>
+            </li>
+            <li v-if="!isLoggedIn" key="" class="nav-item">
+              <a class="nav-link" href="/signup">Signup</a>
+            </li>
+            <li v-if="!isLoggedIn" class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li v-if="isLoggedIn" class="nav-item">
+              <a class="nav-link" href="/logout">Logout</a>
             </li>
           </ul>
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown
+          </a>
+
           <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-success" type="submit">Search</button>
@@ -70,5 +81,20 @@
 
   <router-view />
 </template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  watch: {
+    $route: function () {
+      this.isLoggedIn = !!localStorage.jwt;
+    },
+  },
+};
+</script>
 
 <style></style>
